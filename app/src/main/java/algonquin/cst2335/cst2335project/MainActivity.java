7b946697fcb0e4afc1fdedf8d7481535513e4b93
Franklin Yu <franklinyu777@gmail.com>
2021-07-21 00:15:17 -0400

    Milestone1: Adding XML: MovieSearch TextView EditText Button RecyclerView. Adding MovieSearch: SharedPreferences

--- app/src/main/java/algonquin/cst2335/cst2335project/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/cst2335project/MainActivity.java
@@ -2,13 +2,30 @@ package algonquin.cst2335.cst2335project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myMovieButton = findViewById(R.id.MovieButton);
+        myMovieButton.setOnClickListener( clk->{
+            Intent nextPage = new Intent(MainActivity.this, MovieSearch.class);
+            startActivity(nextPage);
+        });
+
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        Log.d( TAG, "Message");
     }
 }
