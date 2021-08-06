7a8f11461e99c57a47da1dd8dee9dc4a1e4f1faf
davidhongyangliu <davidhongyangliu@gmail.com>
2021-08-06 00:35:49 -0400

    before Ashley finish

--- app/src/main/java/algonquin/cst2335/cst2335project/ChargingDetailFragment.java
+++ app/src/main/java/algonquin/cst2335/cst2335project/ChargingDetailFragment.java
@@ -10,29 +10,5 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 public class ChargingDetailFragment extends Fragment {
-    CarMainActivity.ChargingListItem chosenLocation;
-    int chosenPosition;
-    public ChargingDetailFragment(CarMainActivity.ChargingListItem location, int position){
-        chosenLocation = location;
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View detailsView = inflater.inflate(R.layout.charging_detail,container,false);
 
-
-        ImageView imageView = detailsView.findViewById(R.id.imageView);
-        TextView lacationView = detailsView.findViewById(R.id.loacationTitle);
-        TextView latitudeView = detailsView.findViewById(R.id.latitudeView);
-        TextView longitudeView = detailsView.findViewById(R.id.longitudeView);
-        TextView telView = detailsView.findViewById(R.id.telView);
-
-        lacationView.setText("Location Title: " + chosenLocation.locationTitle);
-        latitudeView.setText("Latitude: " + chosenLocation.latitude);
-        longitudeView.setText("Longitude: " + chosenLocation.longitude);
-        telView.setText("Telphone Number: " +chosenLocation.telNumber);
-
-
-
-        return detailsView;
-    }
 }
